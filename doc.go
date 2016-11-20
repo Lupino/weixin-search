da4bea618ed546964ed330dc458c4c706ddb0681
Lupino <lmjubuntu@gmail.com>
2016-11-20 21:08:06 +0800

    update result id to uri

--- doc.go
+++ doc.go
@@ -7,7 +7,7 @@ import (
 
 // Document defined common document
 type Document struct {
-	ID        string   `json:"id"`
+	ID        string   `json:"uri"`
 	Title     string   `json:"title,omitempty"`
 	Content   string   `json:"content,omitempty"`
 	Tags      []string `json:"tags,omitempty"`
@@ -17,7 +17,7 @@ type Document struct {
 // FieldMap defined the interface for binding form
 func (doc *Document) FieldMap(_ *http.Request) binding.FieldMap {
 	return binding.FieldMap{
-		&doc.ID:        binding.Field{Form: "id", Required: true},
+		&doc.ID:        binding.Field{Form: "uri", Required: true},
 		&doc.Title:     binding.Field{Form: "title", Required: true},
 		&doc.Content:   binding.Field{Form: "content", Required: false},
 		&doc.Tags:      binding.Field{Form: "tags", Required: false},
@@ -26,7 +26,7 @@ func (doc *Document) FieldMap(_ *http.Request) binding.FieldMap {
 }
 
 type hitResult struct {
-	ID        string              `json:"id"`
+	ID        string              `json:"uri"`
 	Fragments map[string][]string `json:"fragments"`
 	Score     float64             `json:"score"`
 }
