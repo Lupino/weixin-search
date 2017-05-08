9d3027338cbea5a6b4e5a6ebb8e84c855a594a71
Lupino <lmjubuntu@gmail.com>
2017-05-08 15:38:50 +0800

    update image and test

--- crawl.go
+++ crawl.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -32,11 +37,14 @@ func doCrawl(uri string) (meta map[string]string, err error) {
 	}
 	doc.Find("script").Each(func(i int, s *goquery.Selection) {
 		text = s.Text()
-		if reMeta.MatchString(text) {
-			match = reMeta.FindStringSubmatch(text)
-			meta[match[1]] = extractData(match[2])
 		}
 	})
+
+	if cover, ok := meta["msg_cdn_url"]; ok {
+		if file, err := uploadImage(cover, "jpg"); err == nil {
+			meta["msg_cdn_url"] = fileUrl(file)
+		}
+	}
 
 	contentElement := doc.Find("#js_content")
 
@@ -44,15 +52,20 @@ func doCrawl(uri string) (meta map[string]string, err error) {
 		var (
 			imgUrl string
 			ok     bool
+			file   File
+			tp     string
 		)
 
 		if imgUrl, ok = s.Attr("data-src"); !ok {
 			imgUrl, _ = s.Attr("src")
 		}
+		tp = s.AttrOr("data-type", "jpg")
+		if file, err = uploadImage(imgUrl, tp); err == nil {
+			imgUrl = fileUrl(file)
+		}
 		s.SetAttr("src", imgUrl)
 		s.RemoveAttr("data-src")
 		s.RemoveAttr("data-s")
-		s.RemoveAttr("data-type")
 		s.RemoveAttr("data-ratio")
 		s.RemoveAttr("data-w")
 	})
@@ -61,5 +74,59 @@ func doCrawl(uri string) (meta map[string]string, err error) {
 		return
 	}
 	meta["msg_content"] = strings.Trim(html, " \n\r\t")
+	return
+}
+
+// {"extra":{"height":296,"size":23903,"width":640,"name":"abc.jpg","type":"image/jpeg","ext":"jpg"},"bucket":"data/33c6615d7aaf88ff2ad1","key":"261645BACE6E14F328DEEE46B6057DECE95482F7","created_at":1494222465,"id":2}
+type Extra struct {
+	Ext string `json:"ext"`
+}
+
+type File struct {
+	Key   string `json:"key"`
+	Extra Extra  `json:"extra"`
+}
+
+func fileUrl(file File) string {
+	return articleHost + "/" + file.Key + "." + file.Extra.Ext + "?key=" + articleKey
+}
+
+func uploadImage(imgUrl, tp string) (file File, err error) {
+	var (
+		req    *http.Request
+		rsp    *http.Response
+		imgRsp *http.Response
+		raw    []byte
+		url    = articleHost + "/api/upload/?fileName=abc." + tp
+	)
+
+	if imgRsp, err = http.Get(imgUrl); err != nil {
+		return
+	}
+	if imgRsp.StatusCode != 200 {
+		err = errors.New("Request image failed.")
+		return
+	}
+	if raw, err = ioutil.ReadAll(imgRsp.Body); err != nil {
+		return
+	}
+	defer imgRsp.Body.Close()
+	if req, err = http.NewRequest("PUT", url, bytes.NewReader(raw)); err != nil {
+		return
+	}
+	if err = filledRequestHeaderWithRaw(req); err != nil {
+		return
+	}
+	if rsp, err = http.DefaultClient.Do(req); err != nil {
+		return
+	}
+	if rsp.StatusCode != 200 {
+		err = errors.New("Upload image failed.")
+		return
+	}
+	decoder := json.NewDecoder(rsp.Body)
+	if err = decoder.Decode(&file); err != nil {
+		return
+	}
 	return
 }
--- crawl_test.go
+++ crawl_test.go
@@ -9,7 +9,25 @@ func TestDoCrawl(t *testing.T) {
 	var (
 		uri = "https://mp.weixin.qq.com/s/0xRVDtKH9DCIffyMh4ha3w"
 	)
+	articleHost = "http://127.0.0.1:3300"
+	articleKey = "33c6615d7aaf88ff2ad1"
+	articleSecret = "4de6cb3779eedbde19f794697d519c122894357a73be5a481467be5769948656"
 	meta, err := doCrawl(uri)
 	fmt.Printf("meta=%s\n", meta)
 	fmt.Printf("err=%v\n", err)
+}
+
+func TestUploadFile(t *testing.T) {
+	var (
+		file   File
+		err    error
+		imgUrl = "http://mmbiz.qpic.cn/mmbiz_png/w9Cccd1M0afktEUibWQQoSU4UONkPMMUHIHOJMibe2ibhxFEn2KpjgGh9350GW0rREwHjcicdPrQskuJN7kqGydH9A/640?wx_fmt=png"
+	)
+	articleHost = "http://127.0.0.1:3300"
+	articleKey = "33c6615d7aaf88ff2ad1"
+	articleSecret = "4de6cb3779eedbde19f794697d519c122894357a73be5a481467be5769948656"
+	if file, err = uploadImage(imgUrl, "jpg"); err != nil {
+		t.Fatalf("uploadImage() failed (%s)", err)
+	}
+	fmt.Printf("image: %s\n", fileUrl(file))
 }
